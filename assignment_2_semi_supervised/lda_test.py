ebf5f74d749d54a1cfecbc6984111fbc00858e22
Torjus Haukom <torjuh@stud.ntnu.no>
2018-03-11 19:04:55 +0100

    Remove .txt files and files containing library exploration

--- assignment_2_semi_supervised/lda_test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-
-
-X = np.random.randn(9,9)
-y = np.random.randint(0,1, 9)
-
-clf = LinearDiscriminantAnalysis()
-clf.fit(X, y)
